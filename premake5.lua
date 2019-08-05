0cf877998cf77dcaacc6cf6f83c99241ffb6d105
SanSan <1036692596@qq.com>
2019-08-05 22:45:21 +0800

    refactor: modify premake file

--- premake5.lua
+++ premake5.lua
@@ -1,29 +1,33 @@
-
 project "ImGui"
-kind "StaticLib"
-language "C++"
-
-targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-objdir ("bin-Intermediate/" .. outputdir .. "/%{prj.name}")
-
-files
-{
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
-}
-
-filter "system:windows"
-    systemversion "latest"
+    kind "StaticLib"
+    language "C++"
     cppdialect "C++17"
-    staticruntime "On"
-    
-filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-Intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+		runtime "Debug"
+        symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+        optimize "on"
